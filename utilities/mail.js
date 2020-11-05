1bd2994d9b64483285669d56f3f7ca40f2e70ed7
tanmay <tanmay@gkmits-MacBook-Air.local>
2020-11-05 14:35:25 +0530

    added send mail functionality

---
+++ utilities/mail.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: process.env.email_host,
+  port: process.env.email_port,
+  service: process.env.email_service,
+  secure: process.env.email_secure,
+  auth: {
+    user: process.env.email_id,
+    pass: process.env.email_pass
+  }
+});
+
+const sendMail = async function (data, type) {
+  let template;
+  const mailOptions = {
+    from: process.env.email_from,
+    to: data.to.join(),
+    subject: data.subject || '',
+    text: data.text || ''
+  };
+  console.log(mailOptions);
+
+  if (template) {
+    mailOptions.html = template;
+  }
+
+  const mail = await transporter.sendMail(mailOptions);
+  return mail;
+};
+
+module.exports = {
+  sendMail
+};
