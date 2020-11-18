d2454b5fdd32a8eb9abf3d1e27c181c45636f53a
tanmay <tanmay@gkmits-MacBook-Air.local>
2020-11-18 13:13:26 +0530

    removed unnecessary comments

--- modules/events/events.routes.js
+++ modules/events/events.routes.js
@@ -1,7 +1,5 @@
 const path = require('path');
 
-// const rules = require(path.resolve('./modules/posts/posts.validator'));
-// const auth = require(path.resolve('./utilities/auth'));
 const eventController = require(path.resolve('./modules/events/events.controller'));
 
 module.exports = function (router) {
