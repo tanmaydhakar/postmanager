1424609d213c336b99afad9d5081a57433e8f341
tanmay <tanmay@gkmits-MacBook-Air.local>
2020-11-18 13:15:23 +0530

    updated events controller

--- modules/events/events.controller.js
+++ modules/events/events.controller.js
@@ -1,17 +1,10 @@
 const path = require('path');
 
-const errorHandler = require(path.resolve('./utilities/errorHandler'));
 const events = require(path.resolve('./utilities/events'));
 
 const getOauth = async function (req, res) {
-  try {
-    console.log('Scheduling Events');
-    await events.getEvents(req.query.code);
-    return res.status(200).json({ status: 'Scheduling Events' });
-  } catch (error) {
-    const errorResponse = errorHandler.getErrorMessage(error);
-    return res.status(errorResponse.statusCode).json({ message: errorResponse.message });
-  }
+  res.status(200).json({ status: 'Scheduling Events' });
+  return events.getEvents(req.query.code);
 };
 
 module.exports = {
